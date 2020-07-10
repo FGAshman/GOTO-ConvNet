9dffbae99a2e11e36dd04e3ca387440422a5b724
FGAshman <50771374+FGAshman@users.noreply.github.com>
2020-07-10 15:13:33 +0100

    Updated file to a new cleaner code format

--- source_detection.py
+++ source_detection.py
@@ -1,83 +1,133 @@
-import pandas, pdb, time, warnings, astropy
-import matplotlib.pyplot as plt
+################################# IMPORTS #################################
+import pandas, time, astropy, sep, skimage, os, cv2
 import numpy as np
-from numpy.polynomial.polynomial import polyval2d
-from scipy import optimize
-from pylab import meshgrid
-from astropy.stats import sigma_clipped_stats, sigma_clip, gaussian_fwhm_to_sigma, mad_std
+from astropy.stats import gaussian_fwhm_to_sigma, mad_std
 from astropy.io import fits
-from astropy.visualization import SqrtStretch
-from astropy.visualization.mpl_normalize import ImageNormalize
 from astropy.convolution import Gaussian2DKernel
 from astropy.nddata import Cutout2D
 from astropy import units as u
-from astropy.visualization import simple_norm
 from astropy.wcs import WCS
-from astropy.utils.data import get_pkg_data_filename
-from astropy.table import Table, Column
-from astropy.modeling import models, fitting
-from photutils import find_peaks, CircularAperture, source_properties, deblend_sources, detect_sources
-from pylab import meshgrid,cm,imshow,contour,clabel,colorbar,axis,title,show
-from astropy.coordinates import Angle, SkyCoord, match_coordinates_sky
-import sep
-import skimage
-import os
-import cv2
+from astropy.table import Table
+from photutils import source_properties, deblend_sources, detect_sources
+from astropy.coordinates import Angle, SkyCoord
 
+################################# CLASS AND FUNCTIONS #################################
+class SourceDetection:
 
-def prepare_data():
+	def background_reduction(imgPath):
+		'''
+		Performing a background reduction on the fits images
+		Arguments:
+			imgFile: Path to the fits file that is requiring background reduction
 
-    def create_table(imgFile,csv_name):
+		'''
+
         print('Opening fits file and subtracting background')
         start = time.time()
-        hdu1 = fits.open(imgFile) #opening the header of the major fits file
-        hdr = hdu1[1].header
+
+        #opening the header of the major fits file
+        hdu1 = fits.open(imgPath) 
+
         #obtaining a signal to noise ratio of the image, can be used instead of calculating a background for detection
         data = hdu1[1].data
+        hdr = hdu1[1].header
         var = hdu1[3].data
         sn_map = data/np.sqrt(var)
+
         #remove background from image - code from sep tutorial. - https://sep.readthedocs.io/en/v1.0.x/tutorial.html
         bkg = sep.Background(data)
         bkg_image = bkg.back()
         bkg_rms = bkg.rms()
-        data_sub = data - bkg #background subtracted data.
+        
+        print(f"Performed background reduction in {time.time() - start}")
 
-        print('Finding sources using photutils, time =', time.time()-start)
-        median = np.median(data_sub)
+        # Returning the background subtracted image
+        subtracted_image = data - bkg
+        return subtracted_image
+
+    def find_sources(data_sub, x_size = 5, y_size = 5, npixels = 10, connectivity = 8):
+    	'''
+		Using photutils to detect the sources within the full fits files
+
+		Arguments:
+			data_sub: Background subtracted fits file
+
+		Optional Arguments:
+			x_size: x extent of the kernel which slides over the image to detect the sources 
+					-- defaults to 5 pixels
+			y_size: y extent of the kernel which slides over the image to detect the sources 
+					-- defaults to 5 pixels
+			n_pixels: number of connected pixels that are greater than the threshold to count a source 
+					-- defaults to 10 pixels
+			connectivity: The type of pixel connectivity used in determining how pixels are grouped into a detected source.
+					-- defaults to 8 pixels which touch along their edges or corners.
+
+    	'''
+
+    	print('Finding sources using photutils')
+    	start = time.time()
+
+    	median = np.median(data_sub)
         std = mad_std(data_sub)
+
         threshold = bkg + (5.0 * bkg_rms)
         sigma = 5.0 * gaussian_fwhm_to_sigma
-        kernel = Gaussian2DKernel(sigma, x_size = 5, y_size = 5) #kernel defaults to 8*stddev
+        kernel = Gaussian2DKernel(sigma, x_size = x_size, y_size = y_size) # Kernel defaults to 8*stddev
         kernel.normalize()
-        segmented_image = detect_sources(data_sub, threshold, npixels = 10, filter_kernel = kernel, connectivity = 8)
-        print('Deblending sources, time =', time.time()-start)
-        segmented_image_deblend = deblend_sources(data_sub, segmented_image, npixels = 10, filter_kernel = kernel, connectivity = 8)
+
+        segmented_image = detect_sources(data_sub, threshold, npixels = npixels, filter_kernel = kernel, connectivity = connectivity)
+        segmented_image_deblend = deblend_sources(data_sub, segmented_image, npixels = npixels, filter_kernel = kernel, connectivity = connectivity)
 
         cat = source_properties(data_sub, segmented_image_deblend)
+
+        # Getting values of the individual stars to place into a table
         x_pos = cat.xcentroid.value
         y_pos = cat.ycentroid.value
         area = cat.area.value
         max_pixel_val = cat.max_value
         ids = cat.id
 
-        print('Comparing sources to galaxy zoo via their RA and DEC')
-        #get world coordinate data from header of image.
+        return ids, x_pos, y_pos, area, max_pixel_val
+
+    def compare_to_catalogue(hdr, x_pos, y_pos, catalogue_path, ids, area, max_pixel_val, csv_name):
+    	'''
+		Creates a csv table of the details of the detected sources. Compares to a chosen catalogue to determine what
+		type of astronomical object is present.
+
+		Arguments:
+			hdr: Header of the major fits file
+			x_pos: List of the x positions of the objects that have been detected in the image
+			y_pos: List of the y positions of the objects that have been detected in the image  
+			catalogue_path: A csv file of an astronomical survey that contains RA and DEC of sources. Must be sources that
+							are within the same area of sky in order to correctly determine what object was detected.
+							For the project the Galaxy Zoo 2 catalogue was used
+			ids: IDs of the sources determined by source detection
+			area: Area of the detected source determined by source detection
+			max_pixel_val: Maximum pixel value of the source determined by source detection
+			csv_name: Name of the file path that the table will be saved to
+
+    	'''
+
+    	#get world coordinate data from header of image.
         wcs = WCS(hdr)
-        ra,dec = wcs.all_pix2world([x_pos],[y_pos],1) #get RA and DEC at pixel positions for each source using wcs
+
+        #get RA and DEC at pixel positions for each source using wcs
+        ra, dec = wcs.all_pix2world([x_pos],[y_pos],1) 
         ra = ra[0]
         ra = Angle(ra, u.deg)
         ra_goto = ra.hour
         dec_goto = dec[0]
 
-        galzoo = pandas.read_csv('gz2_hart16.csv')
+        galzoo = pandas.read_csv(catalogue_csv)
         ra = galzoo.ra
         dec = galzoo.dec
         dec = Angle(dec, u.degree)
         ra = Angle(ra, u.degree)
-        dec = dec.deg
-        ra = ra.deg
+        dec_galzoo = dec.deg
+        ra_galzoo = ra.deg
 
-        galaxy_type = galzoo.gz2_class#get the type of galaxy from gal zoo
+        #get the type of galaxy from gal zoo
+        galaxy_type = galzoo.gz2_class
         galaxy_type_simple = []
         for i in range(len(galaxy_type)):
             test=''.join([x[0] for x in galaxy_type[i].split()])
@@ -88,23 +138,27 @@ def prepare_data():
         dec_goto = Angle(dec_goto,u.deg)
         dec_goto = dec_goto.deg
 
-        from astropy.coordinates import SkyCoord
         c = SkyCoord(ra=ra_goto*u.degree, dec=dec_goto*u.degree)
         catalog = SkyCoord(ra=ra*u.degree, dec=dec*u.degree)
         idx,d2d,d3d = c.match_to_catalog_sky(catalog)
         d2d_degree = d2d.degree
-
-
-        numb_stars = 0 #keep track of number of galaxies and stars in the image, just to know.
+    
+    	# Creating a table that compares the detected stars with the catalogue stars
+    	
+    	# Keeping track of number of galaxies and stars in the image
+    	numb_stars = 0 
         numb_spiral = 0
         numb_ellip = 0
 
-        actual_galaxies = [] #empty array to and true and false for each detected source.
+        # Empty arrays for each detected source.
+        actual_galaxies = [] 
         actual_galaxy_index = []
         galaxy_id = []
         imaged_galaxy_types = []
         for i in range(len(d2d)):
-            if d2d_degree[i] <= 0.000555556: #If distance between source and galaxy in galzoo is <1 arc_sec.
+
+        	#If distance between source and galaxy in galzoo is <1 arc_sec.
+            if d2d_degree[i] <= 0.000555556: 
                 actual_galaxies.append(True)
                 actual_galaxy_index.append(i)
                 galaxy_id.append(idx[i])
@@ -119,58 +173,91 @@ def prepare_data():
         table = Table([ids, x_pos, y_pos, area, max_pixel_val, ra_goto ,dec_goto ,actual_galaxies ,imaged_galaxy_types], 
             names = ('id', 'x_pixel', 'y_pixel', 'area', 'max_pixel_val', 'RA' ,'Dec' ,'actual_galaxy' ,'galaxy_type'))
         table.write(csv_name, format='csv',overwrite=True)
-        print('Created a csv file, according to our source detection there are {0} stars, and according to galaxy zoo2 database there are {1} spiral and {2} elliptical galaxies within the image'.format(numb_stars,numb_spiral,numb_ellip)) 
 
-    def cutout_sources(imgFile,data,cutout_folder,number):
-        hdu1 = fits.open(imgFile) #opening the header of the major fits file
-        img = hdu1[1].data
-        hdr = hdu1[1].header
-        bkg = sep.Background(img)
-        bkg_image = bkg.back()
-        bkg_rms = bkg.rms()
-        img_sub = img - bkg #background subtracted data.
+        print(f'CSV file created')
+        print(f'Total number of stars: {numb_stars}')
+        print(f'Total number of spiral galaxies: {numb_spiral}')
+        print(f'Total number of elliptical galaxies: {numb_ellip}') 
 
-        data = pandas.read_csv(data)
-        ra = data.RA
+    def cutout_sources(img, csv_table, cutout_folder, file_number, IMG_SIZE = 30):
+    	'''
+    	Creates individual cutouts of the sources detected of a selected size, and places
+    	the resulting fits files in folders ready to have analysis performed on them ready
+    	for feeding to the Convolutional Neural Network
+
+    	Arguments:
+    		img: The background subtracted image with detected sources in.
+    		csv_table: Table of detected sources with full pixel information
+    		cutout_folder: Base folder that the cutout images will be saved in.
+    		file_number: number of the base file that the cutouts are taken from - primarily used to iterate through a list
+
+    	Optional Arguments:
+    		IMG_SIZE: x and y dimensions of the cutout images in pixels
+    				--- defaults to 30
+    	'''
+
+    	# Extracting the data from the created table
+    	data = pandas.read_csv(csv_table)
+    	ra = data.RA
         dec = data.Dec
         x_pos = data.x_pixel
         y_pos = data.y_pixel
         galaxy_type = data.galaxy_type
         area = data.area
         max_pix_val = data.max_pixel_val
-        IMG_SIZE = 30
+
+        # Creating the cutouts
         start = time.time()
         if 'E' or 'S' in galaxy_type:
-            print('cutting out individual images from the background subtracted image, saving their RA and DEC')
-            for i in range(len(x_pos)): #loop through all the sources.
-                if  (IMG_SIZE/2) < x_pos[i] < img.shape[1]-(IMG_SIZE/2) and (IMG_SIZE/2) < y_pos[i] < img.shape[0]-(IMG_SIZE/2): #if sources are within 30 pixels of the edge ignore, so cutouts are centred
-                    cutout_img = astropy.nddata.Cutout2D(img_sub,position=(x_pos[i],y_pos[i]),size=(IMG_SIZE,IMG_SIZE)).data #cut out 50x50 image located at x position and y posit
-                    hdu = fits.PrimaryHDU(cutout_img) #get data (in fits form) from the cutout
-                    hdu_header = hdu1[0].header #get header (empty atm)
-                    hdu.header.set('RA',ra[i]) #set header of each cutout to have RA and DEC
+            print('Cutting out individual images from the background subtracted image, saving their RA and DEC')
+            for i in range(len(x_pos)):
+            	# If sources are within IMG_SIZE / 2 pixels of the edge ignore, so cutouts are centred
+                if  (IMG_SIZE / 2) < x_pos[i] < img.shape[1] - (IMG_SIZE / 2) and (IMG_SIZE / 2) < y_pos[i] < img.shape[0] - (IMG_SIZE / 2): 
+                    cutout_img = astropy.nddata.Cutout2D(img_sub, position = (x_pos[i], y_pos[i]), size = (IMG_SIZE, IMG_SIZE)).data
+                    
+                    # Get data (in fits form) from the cutout
+                    hdu = fits.PrimaryHDU(cutout_img) 
+                    hdu_header = hdu1[0].header
+
+                    # Set header of each cutout to have RA and DEC
+                    hdu.header.set('RA',ra[i])
                     hdu.header.set('DEC',dec[i])
+
+                    # Saving the fits files to a folder in a specific format
+
+                    ################################ GALAXIES ################################
                     if galaxy_type[i] == 'E':
-                        hdu.writeto('{0}/elliptical_galaxies_original/elliptical_galaxy{1}_{2}.fits'.format(cutout_folder,number,i+1),overwrite = True)
-                        hdu.writeto('{0}/all_galaxies_original/galaxy{1}_{2}.fits'.format(cutout_folder,number,i+1),overwrite = True) #to create a file with all galaxies in it...    
+                        hdu.writeto(f"{cutout_folder}/elliptical_galaxies_original/elliptical_galaxy{file_number}_{i + 1}.fits", overwrite = True)
+                        hdu.writeto(f"{cutout_folder}/all_galaxies_original/galaxy{file_number}_{i + 1}.fits",overwrite = True) #to create a file with all galaxies in it...    
                     if galaxy_type[i] == 'S':
-                        hdu.writeto('{0}/spiral_galaxies_original/spiral_galaxy{1}_{2}.fits'.format(cutout_folder,number,i+1),overwrite = True)
-                        hdu.writeto('{0}/all_galaxies_original/galaxy{1}_{2}.fits'.format(cutout_folder,number,i+1),overwrite = True)
-                    if area[i] < 75 or np.sqrt(((x_pos[i]-x_pos[i-1])**2 + (y_pos[i]-y_pos[i-1])**2)) < np.sqrt((IMG_SIZE/2)**2 + (IMG_SIZE/2)**2):
-                        hdu.writeto('{0}/artefacts/source{1}_{2}.fits'.format(cutout_folder,number,i+1), overwrite=True) 
+                        hdu.writeto(f"{cutout_folder}/spiral_galaxies_original/spiral_galaxy{file_number}_{i + 1}.fits", overwrite = True)
+                        hdu.writeto(f"{cutout_folder}/all_galaxies_original/galaxy{file_number}_{i + 1}.fits", overwrite = True)
+
+                    # Attemps to remove any anomalous objects (artefacts) from the pool of images used for machine learning
+                    if area[i] < 75 or np.sqrt(((x_pos[i]-x_pos[i - 1])**2 + (y_pos[i] - y_pos[i - 1])**2)) < np.sqrt((IMG_SIZE / 2)**2 + (IMG_SIZE/2)**2):
+                        hdu.writeto(f"{cutout_folder}/artefacts/source{file_number}_{i + 1}.fits", overwrite=True) 
                     else:
-                        hdu.writeto('{0}/stars/star{1}_{2}.fits'.format(cutout_folder,number,i+1),overwrite = True) #write out the cuts to the same file the code is saved to, will overwrite files with the same name.                    
+                    	################################# STARS ################################
+                        hdu.writeto(f"{cutout_folder}/stars/star{number}_{i + 1}.fits",overwrite = True)                   
         else: print('No galaxy present in the image')
-        print('Finished cutting out images, time = ', time.time()-start)
 
-    for x in range(13):
-        for i in range(13):
-            try:
-                create_table('142/{0},{1}.fits'.format(x,i),'142_data/source_{0},{1}.csv'.format(x,i))
-                print('Currently finding sources and creating csv files for image',x,i)
-                cutout_sources('142/{0},{1}.fits'.format(x,i),'142_data/source_{0},{1}.csv'.format(x,i),'142_cutouts',(x*13)+i)
-                print('Currently creating cutouts for image',x,i)
-            except Exception as e:
-                pass 
+        print(f"Finished cutting out images, time = {time.time() - start}")
 
-prepare_data()
+
+################################ USAGE ################################
+CATALOGUE_PATH = 'gz2_hart16.csv'
+
+def prepare_data(imgPath, csv_table, cutout_folder, NUMBER_OF_BASE_IMAGES = 13, BASE_FILE_PATH = '142/', BASE_DATA_PATH = '142_data/'):
+	for i in range(NUMBER_OF_BASE_IMAGES):
+		for j in range(NUMBER_OF_BASE_IMAGES):
+			bkg_reduced_image = SourceDetection.background_reduction(BASE_FILE_PATH + f"{i}_{j}.fits")
+			ids, x_pos, y_pos, area, max_pixel_val = SourceDetection.find_sources(bkg_reduced_image)
+
+			# Opening up the header of the background reduced image      
+        	hdu1 = fits.open(bkg_reduced_image) 
+        	hdr = hdu1[1].header
+
+			SourceDetection.compare_to_catalogue(hdr, x_pos, y_pos, CATALOGUE_PATH, ids, area, max_pixel_val, BASE_DATA_PATH + f"/source{i}_{j}")
+
+			SourceDetection.cutout_sources(bkg_reduced_image, csv_table, cutout_folder, (i * NUMBER_OF_BASE_IMAGES) + j)
 
