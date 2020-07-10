f243a3cd965f650c1c1fed6065364f540a365f6a
FGAshman <50771374+FGAshman@users.noreply.github.com>
2020-07-10 15:15:34 +0100

    Code Refactor

--- create_training_data.py
+++ create_training_data.py
@@ -1,52 +1,20 @@
 import numpy as np 
-import matplotlib.pyplot as plt
-import astropy
-from astropy.utils.data import download_file
+import astropy, os, time, random, pickle
 from astropy.io import fits
-import os
-import cv2
-import tensorflow as tf
-from astropy.modeling.models import Rotation2D
-from scipy import misc
-from scipy.ndimage import rotate
-from scipy.misc import imrotate, imsave
-import os, pandas, time, random
-import shutil, pdb
-from astropy.visualization import ImageNormalize, MinMaxInterval
 
-def move_files(path, destination, rand_numb):
+################################# FUNCTIONS #################################
+
+def create_training_data(DATADIR, CATEGORIES):
 	'''
-	Moves a random selection of files from a selected path to another selected folder (folder must already be created)
-	Arguements: 
-		path: Directory which contains the folders which are to be moved
-		destination: Directory which the folders are to moved to
-		rand_numb: the proportion of files which are moved during the function
+	Compiles the images into a list that can be fed into the machine learning algorithm. Also includes normalization of the
+	images and randomises the list to aid in training the algorithm
 
+	Arguments:
+		DATADIR: Path to the where the image data is located
+		CATEGORIES: A list of the different labels of objects to be trained on i.e. Galaxies and Stars
 	'''
-	files = os.listdir(path)
-	print("Number of files in the path folder:", len(files))
-	src_files = np.random.choice(os.listdir(path), int(len(files)*rand_numb))
-	print("Number of files in the path folder after choosing a random number of files:", len(src_files))
+	training_data = [] 
 
-	for file_name in src_files:
-		full_file_name = os.path.join(path, file_name)
-		if (os.path.isfile(full_file_name)):
-			shutil.move(full_file_name, destination)
-
-#move_files("C:/Users/fredd/OneDrive/Documents/Coding/Python/Machine-learning-project/Project - code/142_cutouts/Stars", 
-#	"C:/Users/fredd/OneDrive/Documents/Coding/Python/Machine-learning-project/Project - code/142_cutouts/stars-overflow", 0.9)
-
-
-
-
-#Not sure if I need to do the splitting up, can do it within tensorflow
-
-DATADIR = "C:/Users/fredd/OneDrive/Documents/Coding/Python/Machine-learning-project/Project-code/142_cutouts"
-CATEGORIES = ["stars", "all_galaxies"]
-
-training_data = [] 
-
-def create_training_data():
 	start = time.time()
 	print("Creating the training set")
 	for category in CATEGORIES:
@@ -55,39 +23,65 @@ def create_training_data():
 		for img in os.listdir(path):
 			hdul = fits.open(os.path.join(path,img))
 			array = hdul[0].data
+
+			# Performing z-scale normalization on the images
 			mean_array = np.mean(array)
 			std_array = np.std(array)
 			img_array = (array - mean_array)/std_array
-			training_data.append([array, class_num])	
-	print("Finished creating the training set in:", time.time() - start)
+			training_data.append([array, class_num])
+
+	print(f"Finished creating the training set in: {time.time() - start}")
+
+	# Randomising the data
+	random.shuffle(training_data)
+
+	return training_data
+
+def save_pickle(file_path, file):
+	'''
+	Simple helper function to save any file as a pickle file
+
+	Arguments:
+		file_path: Path to where the pickle file will be saved
+		file: The actual file to be saved
+	'''
+	
+	pickle_out = open(file_path, "wb")
+	pickle.dump(file, pickle_out)
+	pickle_out.close()
 
 
-create_training_data()
-#print(training_data.shape)
+def data_to_pickle(training_data, FEATURE_SAVE_PATH, LABEL_SAVE_PATH, IMG_SIZE = 30):
+	'''
+	Converting the training data to a pickle file to be fed into the machine learning
 
-#randomising the data
-random.shuffle(training_data)
+	Arguments:
+		training_data: List of randomised, normalized image data to be saved and used in the algorithm
+		FEATURE_SAVE_PATH: Save path of the feature data set
+		LABEL_SAVE_PATH: Save path of the label data set
 
-#X is the feature set, y is the labels
-X = []
-y = []
+	Optional Arguments:
+		IMG_SIZE: x and y dimensions of the images - which end up as the size of the first layer of the CNN
+				--- Defaults to 30  pixels 
+	'''
 
-for features, label in training_data:
-	X.append(features)
-	y.append(label)
+	# X is features (the images), y are labels (what type of object it is)
+	X = []
+	y = []
 
-#Convert X to a numpy array, where the 1 is due to it being greyscale
-X = np.array(X).reshape(-1, 30, 30, 1)
+	for features, label in training_data:
+		X.append(features)
+		y.append(label)
 
-#saving the training data
-import pickle
-pickle_out = open("X_star-galaxy-final.pickle", "wb")
-pickle.dump(X, pickle_out)
-pickle_out.close()
+	#Convert X to a numpy array, where the 1 is due to it being greyscale
+	X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
-pickle_out = open("y_star-galaxy-final.pickle", "wb")
-pickle.dump(y, pickle_out)
-pickle_out.close()
-
+	# Saving the training data as pickle files
+	save_pickle(FEATURE_SAVE_PATH, X)
+	save_pickle(LABEL_SAVE_PATH, y)
 
 
+################################# USAGE #################################
+
+# Creating the training data
+training_data = create_training_data(DATADIR, CATEGORIES)
